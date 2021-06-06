8e73986e48da93e90e1b6d8c6d89108340de5a46
Branson Smith <bransonsmith22@gmail.com>
2021-06-06 10:39:00 -0500

    Add second exclamatino point to home page title

--- bqe_ui/src/pages/HomePage/HomePage.js
+++ bqe_ui/src/pages/HomePage/HomePage.js
@@ -7,7 +7,7 @@ export default class HomePage extends React.Component  {
   render(){
       return(
         <div className="home-page">
-            <PageTitle text="Bible Quiz Extraordinaire!"></PageTitle>
+            <PageTitle text="Bible Quiz Extraordinaire!!"></PageTitle>
             <div className="welcome-message">Welcome to BQE!</div>
         </div>
       );
