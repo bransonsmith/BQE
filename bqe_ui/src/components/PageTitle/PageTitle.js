ab7d2d6712a94e64f2b88ffad6e1527d2ea373bf
Branson Smith <bransonsmith22@gmail.com>
2021-06-09 13:55:59 -0500

    Convert PageTitle to func

--- bqe_ui/src/components/PageTitle/PageTitle.js
+++ bqe_ui/src/components/PageTitle/PageTitle.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './PageTitle.css';
 
-export default class PageTitle extends React.Component  {
-
-  render(){
-      return(
-        <div className="page-title">
-            <h2 className="page-title-text">
-                {this.props.text}
-            </h2>
-        </div>
-      );
-  }
+export default function PageTitle(props) {
+  return(
+    <div className="page-title">
+        <h2 className="page-title-text">
+            {props.text}
+        </h2>
+    </div>
+  );
 }
