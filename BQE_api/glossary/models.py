aeb1415d18845cb1cbef07972c006498849a6a70
pcoxcode <pcoxcode@gmail.com>
2021-06-10 09:57:53 -0500

    added return to models

--- BQE_api/glossary/models.py
+++ BQE_api/glossary/models.py
@@ -5,9 +5,13 @@ from django.db.models.fields import CharField
 # Create your models here.
 class Word(models.Model):
     name = models.CharField(max_length=77)
-
+    def __str__(self):
+        return self.name
+        
 class Entry(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     book = models.CharField(max_length=77)
     chapter = models.IntegerField(default=0)
     verse = models.IntegerField(default=0)
+    def __str__(self):
+        return f'{self.word} {self.book} {self.chapter}: {self.verse}'
