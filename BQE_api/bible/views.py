acefd85711077885e9854f7625a23b0e5f794f4f
Branson Smith <bransonsmith22@gmail.com>
2021-06-13 22:09:30 -0500

    Simplify bible data urls

--- BQE_api/bible/views.py
+++ BQE_api/bible/views.py
@@ -4,16 +4,7 @@ from .services.import_bible_data import import_data
 from .models import *
 from .data.verse_counts import *
 
-def index(request):
-    output = '<style>body: { width: 100%; height: fit_content; display: flex; flex-direction: column;} .out { box-shadow: 2px 2px 7px #565658; width: 700px; max-width: calc(100% - 40px); padding: 20px; margin: 50px auto 0px; min-height: 100px; background-color: #fafafb; } .title { font-size: 20px; width: fit-content; margin: auto auto 15px auto; } .content { font-size: 14px; } .job-start { font-size: 14px; font-weight: bold; background-color: #defcfc; } .job-end { font-size: 14px; font-weight: bold; background-color: #daf8f8; margin-bottom: 40px; } .sub-job { padding-left: 20px; }</style>'
-    output += '<div class="out">'
-    output += '<div class="title">Bible Data Import View</div>'
-    output += '<div class="content">'
-    output += import_data()
-    output += "</div></div>"
-    return HttpResponse(output)
-
-def delete_chapters(request):
+def delete_bible_data(request):
     num = 100
     if len(Chapter.objects.all()) > num:
         Chapter.objects.filter(id__in=list(Chapter.objects.values_list('pk', flat=True)[:num])).delete()
@@ -21,12 +12,12 @@ def delete_chapters(request):
         Chapter.objects.all().delete()
     
     if len(Chapter.objects.all()) > 0:
-        return redirect('delete_chapters')
-    return redirect('delete_bible_data')
+        return redirect('delete_bible_data')
+    return redirect('delete_testaments')
 
-def delete_bible_data(request):
+def delete_testaments(request):
     Testament.objects.all().delete()
-    return HttpResponse('Deleted all Testaments.')
+    return HttpResponse('Deleted all Bible Data.')
 
 def seed_bible_data(request):
     ot = Testament(order=1, name='Old Testament')
@@ -61,7 +52,7 @@ def seed_books(request):
         if num_uploaded > 10:
             return redirect('seed_books')
 
-    return HttpResponse('Seeded all Books.')
+    return HttpResponse('Done Seeding Bible Data.')
 
 def add_chapters(book_num, book, verse_counts):
     output = ''
