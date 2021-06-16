1eabd8b099512fd6305acc529d92a065a830bbe0
Branson Smith <bransonsmith22@gmail.com>
2021-06-16 15:20:53 -0500

    Clean up

--- BQE_api/glossary/models.py
+++ BQE_api/glossary/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
-from django.db.models.fields import CharField
 
-# Create your models here.
 class Word(models.Model):
     name = models.CharField(max_length=77)
     def __str__(self):
--- BQE_api/glossary/urls.py
+++ BQE_api/glossary/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', homeView.as_view(),name='home')
+    path('', index, name='index')
 
 ]
