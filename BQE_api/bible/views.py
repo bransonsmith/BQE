0600a87d3c73fb03406c47c3853ce7bcbbce3b67
Branson Smith <bransonsmith22@gmail.com>
2021-06-16 23:22:46 -0500

    Fix title typo

--- BQE_api/bible/views.py
+++ BQE_api/bible/views.py
@@ -9,7 +9,7 @@ def index(request):
     output = ''
     output = '<style>body: { width: 100%; height: fit_content; display: flex; flex-direction: column;} .out { box-shadow: 2px 2px 7px #565658; width: 700px; max-width: calc(100% - 40px); padding: 20px; margin: 50px auto 0px; min-height: 100px; background-color: #fafafb; } .title { font-size: 20px; width: fit-content; margin: auto auto 15px auto; } .content { font-size: 14px; } .job-start { font-size: 14px; font-weight: bold; background-color: #defcfc; } .job-end { font-size: 14px; font-weight: bold; background-color: #daf8f8; margin-bottom: 40px; } .sub-job { padding-left: 20px; }</style>'
     output += '<div class="out">'
-    output += '<div class="title">{app_name} data</div>'
+    output += f'<div class="title">{app_name} data</div>'
     output += '<div class="content">'
     output += f'To seed the {app_name} data 1) navigate to <a href="./delete_bible">/{app_name}/delete_bible</a> 2) navigate to <a href="./seed_bible">/{app_name}/seed_bible</a>'
     output += "</div></div>"
