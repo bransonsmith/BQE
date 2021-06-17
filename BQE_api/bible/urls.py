0e646d146eb1bf2e8c85179e8877f45ae4929d48
Branson Smith <bransonsmith22@gmail.com>
2021-06-16 23:19:06 -0500

    Standardize data seeding a bit

--- BQE_api/bible/urls.py
+++ BQE_api/bible/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('delete', views.delete, name='delete'),
-    path('seed', views.seed, name='seed'),
+    path('delete_bible', views.delete_bible, name='delete_bible'),
+    path('seed_bible', views.seed_bible, name='seed_bible'),
     path('seed_books', views.seed_books, name='seed_books'),
 ]
--- BQE_api/bible/views.py
+++ BQE_api/bible/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import redirect
 from django.http import HttpResponse
-from .services.import_bible_data import import_data
 from .models import *
 from .data.verse_counts import *
-import os
 
-page_title = 'Bible Data';
 app_name = 'bible';
 
 def index(request):
     output = ''
     output = '<style>body: { width: 100%; height: fit_content; display: flex; flex-direction: column;} .out { box-shadow: 2px 2px 7px #565658; width: 700px; max-width: calc(100% - 40px); padding: 20px; margin: 50px auto 0px; min-height: 100px; background-color: #fafafb; } .title { font-size: 20px; width: fit-content; margin: auto auto 15px auto; } .content { font-size: 14px; } .job-start { font-size: 14px; font-weight: bold; background-color: #defcfc; } .job-end { font-size: 14px; font-weight: bold; background-color: #daf8f8; margin-bottom: 40px; } .sub-job { padding-left: 20px; }</style>'
     output += '<div class="out">'
-    output += '<div class="title">{page_title} Import</div>'
+    output += '<div class="title">{app_name} data</div>'
     output += '<div class="content">'
-    output += f'To seed the {app_name} data 1) navigate to <a href="./delete">/{app_name}/delete</a> 2) navigate to <a href="./seed">/{app_name}/seed</a>'
+    output += f'To seed the {app_name} data 1) navigate to <a href="./delete_bible">/{app_name}/delete_bible</a> 2) navigate to <a href="./seed_bible">/{app_name}/seed_bible</a>'
     output += "</div></div>"
     return HttpResponse(output)
 
-def delete(request):
+def delete_bible(request):
     num_chapters_to_delete_in_each_run = 100
     number_of_chapters_at_start = len(Chapter.objects.all())
     print(f'Deleting {max(num_chapters_to_delete_in_each_run, number_of_chapters_at_start)} chapters.')
@@ -29,12 +26,12 @@ def delete(request):
         Chapter.objects.all().delete()
     
     if len(Chapter.objects.all()) > 0:
-        return redirect('delete')
+        return redirect('delete_bible')
     
     Testament.objects.all().delete()
-    return HttpResponse(f'Deleted all Bible Data.<br>Go to <a href="./seed">bible/seed</a> to seed the bible data (this may take a few minutes).')
+    return HttpResponse(f'Deleted all {app_name} data.<br>Go to <a href="./seed_bible">{app_name}/seed_bible</a> to seed the {app_name} data (this may take a few minutes).')
 
-def seed(request):
+def seed_bible(request):
     ot = Testament(order=1, name='Old Testament')
     ot.save()
     nt = Testament(order=2, name='New Testament')
@@ -48,7 +45,7 @@ def seed_books(request):
     num_books_to_add_in_each_call = 6
     book_count = len(list(Book.objects.all()))
 
-    print(f'Importing Bible - Book count before: {book_count}')
+    print(f'Importing {app_name} - Book count before: {book_count}')
     print(f'Importing {num_books_to_add_in_each_call} more books.')
 
     num_books_added_this_call = 0
@@ -67,8 +64,8 @@ def seed_books(request):
     if book_number < desired_number_of_books:
         return redirect('seed_books')
 
-    print('Seed Bible')
-    return HttpResponse('Done Seeding Bible Data.')
+    print(f'Seeded {app_name}')
+    return HttpResponse(f'Done seeding {app_name} data.')
 
 def add_chapters(book_num, book, verse_counts):
     output = ''
