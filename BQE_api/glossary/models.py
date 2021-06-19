c2a51f67a2dfa8d4b0a337024a96d8c383d1cc98
pcoxcode <pcoxcode@gmail.com>
2021-06-19 13:55:02 -0500

    create Answer model and add answer end point

--- BQE_api/glossary/models.py
+++ BQE_api/glossary/models.py
@@ -12,3 +12,11 @@ class Entry(models.Model):
     verse = models.IntegerField(default=0)
     def __str__(self):
         return f'{self.word} {self.book} {self.chapter}: {self.verse}'
+
+class Answer(models.Model):
+    question_id = models.IntegerField(default=0)
+    book = models.CharField(max_length=77)
+    chapter = models.IntegerField(default=0)
+    verse = models.IntegerField(default=0)
+    def __str__(self):
+        return f'{self.word} {self.book} {self.chapter}: {self.verse}'
