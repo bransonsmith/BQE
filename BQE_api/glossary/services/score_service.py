94ed3ba562de878c827e832939ab153b88557258
Branson Smith <bransonsmith22@gmail.com>
2021-06-23 16:19:22 -0500

    Add more tests

--- BQE_api/glossary/services/score_service.py
+++ BQE_api/glossary/services/score_service.py
@@ -1,11 +1,13 @@
 from ..models import Answer, Entry, Word
 
+#### MAKE THESE MATCH WHATEVER YOU PUT IN TEST FILE AS WELL
+POINTS_IF_BOOK_CORRECT = 10
+POINTS_IF_CHAPTER_CORRECT = 15
+POINTS_IF_VERSE_CORRECT = 25
+
 def score_answer(answer: Answer):
     score = 0
 
-    word = Word.objects.filter(name=answer.word).first()
-    book_entries = Entry.objects.filter(word=word, book=answer.book)
-    if len(list(book_entries)):
-        score += 10
+    ### Your code here
 
     return score
--- BQE_api/glossary/tests.py
+++ BQE_api/glossary/tests.py
@@ -4,6 +4,12 @@ from .services import score_service
 
 class ScoreAnswerTests(TestCase):
 
+    ### SET THESE FOR THE TESTS TO CHECK THESE SPECIFIC VALUES
+    WRONG_SCORE = 0
+    BOOK_SCORE = 10
+    BOOK_AND_CHAPTER_SCORE = 25
+    BOOK_CHAPTER_AND_VERSE_SCORE = 50
+
     # This is the bare minimum, we can add more entries especially to test other parts of the logic.
     @classmethod
     def setUpTestData(cls):
@@ -11,16 +17,58 @@ class ScoreAnswerTests(TestCase):
         word.save()
         entry = Entry(word=word, book='TestBook', chapter=1, verse=1)
         entry.save()
+        entry = Entry(word=word, book='TestBook2', chapter=2, verse=2)
+        entry.save()
+        entry = Entry(word=word, book='TestBook3', chapter=3, verse=3)
+        entry.save()
+        entry = Entry(word=word, book='TestBook2', chapter=1, verse=1)
+        entry.save()
 
     def test_no_part_of_answer_correct(self):
-        expected_score = 0
+        expected_score = self.WRONG_SCORE
         answer = self.get_answer_with_nothing_correct()
         result = score_service.score_answer(answer)
         self.assertIs(result, expected_score)
 
     def test_only_book_correct(self):
-        expected_score = 10
+        expected_score = self.BOOK_SCORE
         answer = self.get_answer_with_only_book_correct()
+        result = score_service.score_answer(answer)
+        self.assertIs(result, expected_score)
+
+    def test_only_book_correct_lowercase(self):
+        expected_score = self.BOOK_SCORE
+        answer = self.get_answer_with_only_book_correct_lowercase()
+        result = score_service.score_answer(answer)
+        self.assertIs(result, expected_score)
+
+    def test_only_book_and_chapter_correct(self):
+        expected_score = self.BOOK_AND_CHAPTER_SCORE
+        answer = self.get_answer_with_only_book_and_chapter_correct()
+        result = score_service.score_answer(answer)
+        self.assertIs(result, expected_score)
+
+    def test_all_correct(self):
+        expected_score = self.BOOK_CHAPTER_AND_VERSE_SCORE
+        answer = self.get_answer_with_all_correct()
+        result = score_service.score_answer(answer)
+        self.assertIs(result, expected_score)
+
+    def test_only_chapter_verse_correct(self):
+        expected_score = self.WRONG_SCORE
+        answer = self.get_answer_with_only_chapter_verse_correct()
+        result = score_service.score_answer(answer)
+        self.assertIs(result, expected_score)
+
+    def test_only_book_and_verse_correct(self):
+        expected_score = self.BOOK_SCORE
+        answer = self.get_answer_with_only_book_and_verse_correct()
+        result = score_service.score_answer(answer)
+        self.assertIs(result, expected_score)
+
+    def test_word_does_not_exist(self):
+        expected_score = self.WRONG_SCORE
+        answer = self.get_answer_with_word_that_is_not_in_db()
         result = score_service.score_answer(answer)
         self.assertIs(result, expected_score)
 
@@ -30,3 +78,24 @@ class ScoreAnswerTests(TestCase):
 
     def get_answer_with_only_book_correct(self):
         return Answer('TestWord', 'TestBook', 0, 0)
+
+    def get_answer_with_only_book_and_chapter_correct(self):
+        return Answer('TestWord', 'TestBook', 1, 0)
+
+    def get_answer_with_all_correct(self):
+        return Answer('TestWord', 'TestBook', 1, 1)
+
+    def get_answer_with_only_book_correct_lowercase(self):
+        return Answer('TestWord', 'testbook', 0, 0)
+
+    def get_answer_with_only_chapter_verse_correct(self):
+        return Answer('TestWord', 'BadBook', 1, 0)
+    
+    def get_answer_with_only_book_and_verse_correct(self):
+        return Answer('TestWord', 'TestBook', 0, 1)
+
+    def get_answer_with_chapter_verse_of_another_entry_correct(self):
+        return Answer('TestWord', 'TestBook', 2, 2)
+
+    def get_answer_with_word_that_is_not_in_db(self):
+        return Answer('BadWord', 'TestBook', 1, 1)
